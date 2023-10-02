518940d5907c5289a636f8ba07a3127ca25482cf
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-10-02 12:49:20 -0300

    fix: router hotels

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
+import { getHotelById, getHotels } from '@/controllers/hotels-controller';
 
 const hotelsRouter = Router();
 
-hotelsRouter.all('/*', authenticateToken).get('/hotels').get('/hotel/:hotelId');
+hotelsRouter.all('/*', authenticateToken).get('/hotels', getHotels).get('/hotel/:hotelId', getHotelById);
 
 export { hotelsRouter };
--- tests/integration/hotels.test.ts
+++ tests/integration/hotels.test.ts
@@ -40,3 +40,28 @@ describe('GET /hotels', () => {
     expect(response.status).toBe(httpStatus.UNAUTHORIZED);
   });
 });
+
+describe('GET /hotels/:', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.get('/hotels');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should responde with status 401 if there is no sessions for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get('/hotels').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+});
